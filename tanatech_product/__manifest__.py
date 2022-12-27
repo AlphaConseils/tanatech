710a3c8ec264bd49d4b6753244cd6bbebdc484df
Warren <nexources@nexources.com>
2022-12-27 14:59:31 +0300

    [FEAT] tanatech_product: product label

---
+++ tanatech_product/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Tanatech Products',
+    'version': '1.2',
+    'category': 'Sales/Sales',
+    'depends': ['product'],
+    'description': """
+This is the base module for managing products and pricelists in Odoo.
+========================================================================
+
+Products support variants, different pricing methods, vendors information,
+make to stock/order, different units of measure, packaging and properties.
+
+Pricelists support:
+-------------------
+    * Multiple-level of discount (by product, category, quantities)
+    * Compute price based on different criteria:
+        * Other pricelist
+        * Cost price
+        * List price
+        * Vendor price
+
+Pricelists preferences by product and/or partners.
+
+Print product labels with barcode.
+    """,
+    'data': [
+        'report/product_label.xml'
+    ],
+    'license': 'LGPL-3',
+}
