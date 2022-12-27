ccc6fee5a78f99e1c6c9a0811f63dac8535f635f
Warren <nexources@nexources.com>
2022-12-27 12:08:59 +0300

    [INIT] tanatech_stock: deliveryslip A5

---
+++ tanatech_stock/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Tanatech Inventory',
+    'version': '1.1',
+    'summary': 'Manage your stock and logistics activities',
+    'description': "",
+    'website': 'https://www.nexources.com/',
+    'depends': ['stock', 'tanatech_base'],
+    'category': 'Inventory/Inventory',
+    'sequence': 25,
+    'data': [
+        # report
+        'report/stock_report_views.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
