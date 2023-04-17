dd985ac5cf972cdf5557a4d5cf0f4bd6d3245c09
Warren <warren@nexources.com>
2023-04-17 09:24:23 +0300

    [ADD] tanatech_base: ng

--- tanatech_base/__manifest__.py
+++ tanatech_base/__manifest__.py
@@ -1,31 +1,26 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Tanatech Base",
-
-    'summary': """
+    "name": "Tanatech Base",
+    "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
+    "description": """
         Long description of module's purpose
     """,
-
-    'author': "Nexources",
-    'website': "http://www.nexources.com",
-
+    "author": "Nexources",
+    "website": "http://www.nexources.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    "category": "Uncategorized",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
+    "depends": ["base"],
     # always loaded
-    'data': [
+    "data": [
         # report
-        'report/a5.xml',
-        'report/external_layout_standard.xml'
+        "report/a5.xml",
+        "report/external_layout_standard.xml",
     ],
+    "assets": {"web.assets_backend": ["tanatech_base/static/src/scss/bg.scss"]},
 }
