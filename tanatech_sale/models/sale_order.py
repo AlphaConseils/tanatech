d07294deb112b6149a1a9043ab344a6da4cb6458
Warren <warren@nexources.com>
2023-04-18 16:43:27 +0300

    [RM] tanatech_sale: depends_context

--- tanatech_sale/models/sale_order.py
+++ tanatech_sale/models/sale_order.py
@@ -14,21 +14,18 @@ class SaleOrder(models.Model):
     is_office_design = fields.Boolean(compute="_compute_is_office_design")
     can_edit_pricelist = fields.Boolean(compute="_compute_can_edit_pricelist")
 
-    @api.depends_context('uid')
     def _compute_is_commercial(self):
         for rec in self:
             rec.is_commercial = self.env.user.has_group(
                 "tanatech_sale.commercial_group"
             )
 
-    @api.depends_context('uid')
     def _compute_is_office_design(self):
         for rec in self:
             rec.is_office_design = self.env.user.has_group(
                 "tanatech_sale.office_design_group"
             )
 
-    @api.depends_context('uid')
     def _compute_can_edit_pricelist(self):
         for rec in self:
             rec.can_edit_pricelist = self.env.user.has_group(
