f5a26662d30b7a707626030cb30f34cd82f2d35a
Warren <warren@nexources.com>
2023-04-18 09:49:57 +0300

    [ADD] tanatech_sale: price list group

--- tanatech_sale/models/sale_order.py
+++ tanatech_sale/models/sale_order.py
@@ -2,20 +2,32 @@
 
 from odoo import models, fields
 
+
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     partner_id = fields.Many2one(required=False)
     partner_invoice_id = fields.Many2one(required=False)
     partner_shipping_id = fields.Many2one(required=False)
     pricelist_id = fields.Many2one(required=False)
-    is_commercial = fields.Boolean(compute='_compute_is_commercial')
-    is_office_design = fields.Boolean(compute='_compute_is_office_design')
+    is_commercial = fields.Boolean(compute="_compute_is_commercial")
+    is_office_design = fields.Boolean(compute="_compute_is_office_design")
+    can_edit_pricelist = fields.Boolean(compute="_compute_can_edit_pricelist")
 
     def _compute_is_commercial(self):
         for rec in self:
-            rec.is_commercial = self.env.user.has_group('tanatech_sale.commercial_group')
+            rec.is_commercial = self.env.user.has_group(
+                "tanatech_sale.commercial_group"
+            )
 
     def _compute_is_office_design(self):
         for rec in self:
-            rec.is_office_design = self.env.user.has_group('tanatech_sale.office_design_group')
+            rec.is_office_design = self.env.user.has_group(
+                "tanatech_sale.office_design_group"
+            )
+
+    def _compute_can_edit_pricelist(self):
+        for rec in self:
+            rec.can_edit_pricelist = self.env.user.has_group(
+                "tanatech_sale.pricelist_group"
+            )
