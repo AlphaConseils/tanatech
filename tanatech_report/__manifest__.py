9e24bdd6e8d8663b1d53175d29be247755eaee1c
nazirah <nazirah@nexources.com>
2024-01-23 18:48:47 +0300

    [ADD] report_assets_pdf into manifest

--- tanatech_report/__manifest__.py
+++ tanatech_report/__manifest__.py
@@ -1,46 +1,49 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "tanatech_report",
-
-    'summary': """
+    "name": "tanatech_report",
+    "summary": """
         change font color in the document""",
-
-    'description': """
+    "description": """
         change font color in the document
     """,
-
-    'author': "Nexources",
-    'website': "http://www.nexources.com",
-
+    "author": "Nexources",
+    "website": "http://www.nexources.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    "category": "Uncategorized",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base','account', 'sale', 'purchase', 'stock','sale_management','tanatech_account','hr_expense'],
-
+    "depends": [
+        "base",
+        "account",
+        "sale",
+        "purchase",
+        "stock",
+        "sale_management",
+        "tanatech_account",
+        "hr_expense",
+    ],
     # always loaded
-    'data': [
-        'report/font_sale_report.xml',
-        'report/font_purchase_report.xml',
-        'report/font_purchaseorder_report.xml',
-        'report/font_stockpicking_report.xml',
-        'report/font_sale_managment.xml',
-        'report/font_report_delivery_document.xml',
-        'report/font_report_account_document.xml',
-        'report/font_report_hr_expense.xml',
-        'report/font_payement.xml',
-        'report/account_report_invoice_document_copy_2.xml'
+    "data": [
+        "report/font_sale_report.xml",
+        "report/font_purchase_report.xml",
+        "report/font_purchaseorder_report.xml",
+        "report/font_stockpicking_report.xml",
+        "report/font_sale_managment.xml",
+        "report/font_report_delivery_document.xml",
+        "report/font_report_account_document.xml",
+        "report/font_report_hr_expense.xml",
+        "report/font_payement.xml",
+        "report/account_report_invoice_document_copy_2.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
-    'assets':{
-        'web.report_assets_pdf':[
-            'tanatech_report/static/report.scss',
+    "assets": {
+        "web.report_assets_common": [
+            "tanatech_report/static/report.scss",
         ]
-    }
+    },
 }
