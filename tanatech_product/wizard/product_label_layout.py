53f8241689376f71a13ebf41628b21d3de25c5be
IdealsonNex <idealson@nexources.com>
2024-02-06 12:23:12 +0300

    [UPDATE] price with taxes in tickets

--- tanatech_product/wizard/product_label_layout.py
+++ tanatech_product/wizard/product_label_layout.py
@@ -22,5 +22,10 @@ class ModelName(models.TransientModel):
         elif 'tanatechxprice' == self.print_format:
             xml_id = 'tanatech_product.label_product_product_tanatech'
             data['price_included'] = True
+            product = self.env['product.template'].browse(self.env.context.get('active_id'))
+            if product.is_product_variant:
+                data['price_tax_included'] = product.taxes_id.compute_all(product.lst_price)['total_included']
+            else:
+                data['price_tax_included'] = product.taxes_id.compute_all(product.list_price)['total_included']
 
         return xml_id, data
