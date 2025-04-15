eb97c6e6e8f1ac354308b4e07e93464ba15216a8
MaximilienNaina <maximilien@nexources.com>
2025-04-15 21:52:54 +0300

    merge

--- bl_security/models/res_users.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    add_item_bl = fields.Boolean(string='blocage ajout BL', default=False)
--- product_pricelist_display/models/product_pricelist.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*
-from odoo import api, fields, models
-
-
-class ProductPricelist(models.Model):
-    _inherit = 'product.pricelist'
-
-    display_in_products = fields.Boolean("Display in product lists")
