f653221da959776992bc38395f590495a9722035
MaximilienNaina <maximilien@nexources.com>
2025-04-15 21:52:43 +0300

    merge

--- tanatech_account_reports/__manifest__.py
+++
@@ -1,7 +0,0 @@
-{
-    "name": "TANATECH - ACCOUNT REPORTS",
-    "version": "15.0.1.0.1",
-    "license": "AGPL-3",
-    "depends": ["account_reports", "tanatech_account"],
-    "data": ["report/report_financial.xml"],
-}
