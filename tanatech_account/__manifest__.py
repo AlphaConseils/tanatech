12918ee56f7ae55d67a2d73c59c5adf7aa6482f3
MaximilienNaina <maximilien@nexources.com>
2025-06-11 22:40:12 +0300

    add group for v18

--- tanatech_account/__manifest__.py
+++ tanatech_account/__manifest__.py
@@ -1,31 +1,31 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Tanatech Invoicing',
-    'version' : '1.2',
-    'summary': 'Invoices & Payments',
-    'sequence': 10,
-    'description': """
+    "name": "Tanatech Invoicing",
+    "version": "1.2",
+    "summary": "Invoices & Payments",
+    "sequence": 10,
+    "description": """
 Invoicing & Payments
 ====================
 The specific and easy-to-use Invoicing system in Odoo allows you to keep track of your accounting, even when you are not an accountant. It provides an easy way to follow up on your vendors and customers.
 
 You could use this simplified accounting in case you work with an (external) account to keep your books, and you still want to keep track of payments. This module also offers you an easy method of registering payments, without having to encode complete abstracts of account.
     """,
-    'category': 'Accounting/Accounting',
-    'website': 'https://www.nexources.com/',
-    'depends' : ['account', 'tanatech_base'],
-    'data': [
-
+    "category": "Accounting/Accounting",
+    "website": "https://www.nexources.com/",
+    "depends": ["account", "tanatech_base"],
+    "data": [
+        # security
+        "security/res_groups.xml",
         # report
-        'report/report_invoice.xml',
-        'report/inherit_report_invoice_document.xml',
-        "report/report_fa_delivery_note.xml"
+        "report/report_invoice.xml",
+        "report/inherit_report_invoice_document.xml",
+        "report/report_fa_delivery_note.xml",
     ],
-    'demo': [
-    ],
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-    'license': 'LGPL-3',
+    "demo": [],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "license": "LGPL-3",
 }
