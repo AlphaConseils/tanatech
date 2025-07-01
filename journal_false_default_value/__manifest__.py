f0d21ae66d572afd2c2ec094a911af6b0feb5ebe
MaximilienNaina <maximilien@nexources.com>
2025-07-01 13:52:39 +0300

    merge

--- journal_false_default_value/__manifest__.py
+++ journal_false_default_value/__manifest__.py
@@ -2,16 +2,15 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Journal False Default Value',
-    'version': '1.2',
-    'summary': 'Sales internal machinery',
-    'description': """
+    "name": "Journal False Default Value",
+    "version": "1.2",
+    "summary": "Sales internal machinery",
+    "description": """
 This module remove journal_id default value on account.
     """,
-    'depends': ['account_accountant', 'sale'],
-    'data': [
-        'views/account_move_views.xml'
-    ],
-    'installable': True,
-    'auto_install': False,
+    "depends": ["account_accountant", "sale"],
+    "data": ["views/account_move_views.xml"],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
 }
