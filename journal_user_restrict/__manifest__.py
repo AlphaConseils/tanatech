c116f61da9a09c227b4fbaaf6056e3b204b5af89
MaximilienNaina <maximilien@nexources.com>
2025-07-01 13:35:26 +0300

    Revert "Merge task#8187 into stage_2"
    This reverts commit 0940e095101edfa900a2aba7cfd02f0b9bf254fe, reversing
    changes made to 6842ff18765590f109d37f5821c37bd44e13116a.

--- journal_user_restrict/__manifest__.py
+++ journal_user_restrict/__manifest__.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    "name": "Journal user restrict",
-    "version": "18.0.1.0.0",
-    "summary": "Restrict journal access for certain users",
-    "sequence": 10,
-    "category": "Accounting/Accounting",
-    "website": "https://www.nexources.com",
-    "depends": ["account", "point_of_sale"],
-    "data": ["security/ir_rule.xml", "views/account_journal_views.xml"],
-    "license": "LGPL-3",
+    'name' : 'Journal user restrict',
+    'version' : '1.1',
+    'summary': 'Restrict journal access for certain users',
+    'sequence': 10,
+    'category': 'Accounting/Accounting',
+    'website': 'https://www.nexources.com',
+    'depends' : ['account'],
+    'data': [
+        'security/ir_rule.xml',
+
+        'views/account_journal_views.xml'
+    ],
+    'license': 'LGPL-3',
 }
