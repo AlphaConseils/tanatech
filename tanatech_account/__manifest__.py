95110a6500412f616609185b27879262a8f92004
hoby-nexources <hoby@naexources>
2026-08-21 10:53:10 +0300

    [FIX] tanatech_sale,tanatech_account: ticket/4846/amount in words

--- tanatech_account/__manifest__.py
+++ tanatech_account/__manifest__.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-{
+{  # noqa: B018
     "name": "Tanatech Invoicing",
     "version": "1.2",
     "summary": "Invoices & Payments",
@@ -23,10 +22,7 @@ You could use this simplified accounting in case you work with an (external) acc
         "report/inherit_report_invoice_document.xml",
         "report/report_fa_delivery_note.xml",
         "views/account_move_views.xml",
+        "report/report_invoice_document_copy_2_inherited.xml",
     ],
-    "demo": [],
-    "installable": True,
-    "application": True,
-    "auto_install": False,
     "license": "LGPL-3",
 }
